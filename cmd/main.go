a13b0d55dc1decf54fbb6486e2025a0447703841
= <=>
2024-01-19 10:08:49 -0500

    First steps added

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,185 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/manifoldco/promptui"
+)
+
+const (
+	Reset           = "\033[0m"
+	Red             = "\033[31m"
+	Green           = "\033[32m"
+	Yellow          = "\033[33m"
+	SecretsFileName = "secrets.json"
+)
+
+type SecretsStruct struct {
+	Changelog time.Time         `json:"changelog"`
+	Secrets   map[string]string `json:"secrets"`
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var secretsData SecretsStruct
+	var secretKey string
+	welcome_logo := `    
+                      | |           | |          | |    
+  ___ _ __ _   _ _ __ | |_ ___   ___| |_ __ _ ___| |__  
+ / __| '__| | | | '_ \| __/ _ \ / __| __/ _ \ /__| '_  \ 
+| (__| |  | |_| | |_) |  | (_) |\__ \  | (_| \__ \ | | |
+ \___|_|   \__, | .__/ \__\___/ |___/\__\__,_|___/_| |_|
+            __/ | |                               
+           |___/|_|                            
+	`
+
+	fmt.Println(Red + welcome_logo + Reset)
+	fmt.Println(
+		Yellow +
+			"Welcome to crypto_stash an open tool for keep secrets secured" +
+			Reset)
+
+	keyPathFlag := flag.String("key", "", "Key for signing secrets")
+	flag.Parse()
+
+	loadSecretKeyFile(&secretKey, *keyPathFlag)
+
+	fmt.Println(secretKey)
+	err := checkExistenceSecrets()
+
+	if err != nil {
+		fmt.Println(Red + "Error in file checking process." + Reset)
+	}
+
+	err = loadSecretsInfo(&secretsData)
+
+	if err != nil {
+		fmt.Println(Red + "Error while loading secrets info process." + Reset)
+	}
+
+	cmd_options := []string{
+		"1) List secrets",
+		"2) Add new secret",
+		"3) Get one secret",
+	}
+
+	prompt := promptui.Select{
+		Label: "Choose what you want to do",
+		Items: cmd_options,
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf(Red+"An unexpected error has ocurred: %v"+Reset, err)
+		os.Exit(0)
+	}
+
+	chosenOption, err := strconv.Atoi(result[:1])
+	switch chosenOption {
+	case 1:
+		listSecrets(secretsData)
+	default:
+		fmt.Println("ToDo")
+	}
+	fmt.Println(chosenOption)
+}
+
+func loadSecretKeyFile(secretKey *string, secretKeyFilePath string) error {
+	_, err := os.Stat(secretKeyFilePath)
+	if !os.IsNotExist(err) {
+		data, err := os.ReadFile(secretKeyFilePath)
+		if err != nil {
+			println(Red+"Error reading secret key file: %v"+Reset, err)
+		}
+		*secretKey = string(data)
+		return nil
+	}
+	fmt.Println("No secret key file were found")
+	cmd_options := []string{
+		"Yes",
+		"No",
+	}
+
+	prompt := promptui.Select{
+		Label: "Want to generate a secret key?",
+		Items: cmd_options,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf(Red+"An unexpected error has ocurred: %v"+Reset, err)
+		return err
+	}
+
+	if result == "No" {
+		os.Exit(0)
+	}
+	_, err = os.Create("key.txt")
+
+	if err != nil {
+		fmt.Printf(Red+"An unexpected error has ocurred: %v"+Reset, err)
+		os.Exit(0)
+	}
+	return nil
+}
+
+func listSecrets(secretsData SecretsStruct) {
+	fmt.Println(Green + "============ Listing secrets ============" + Reset)
+	for key, value := range secretsData.Secrets {
+		fmt.Printf(Yellow+"Name: "+Reset+"%s "+Yellow+"Secret: "+Reset+"%s", key, value)
+	}
+}
+
+func loadSecretsInfo(secretsData *SecretsStruct) error {
+	data, err := os.ReadFile(SecretsFileName)
+
+	if err != nil {
+		fmt.Printf(Red+"An error has ocurred while reading secrets file: %v"+Reset, err)
+		return err
+	}
+
+	err = json.Unmarshal(data, &secretsData)
+
+	if err != nil {
+		fmt.Printf(Red+"An error has ocurred while unmarshal secrets file info: %v"+Reset, err)
+		return err
+	}
+	return nil
+}
+func createInitialSecretsFile() error {
+	secretsTemplate := SecretsStruct{
+		Changelog: time.Now(),
+		Secrets:   make(map[string]string),
+	}
+
+	jsonData, err := json.MarshalIndent(secretsTemplate, "", "    ")
+
+	if err != nil {
+		fmt.Printf(Red+"Error marshalling JSON: %v"+Reset, err)
+		return err
+	}
+
+	err = os.WriteFile(SecretsFileName, jsonData, 0644)
+
+	if err != nil {
+		fmt.Printf(Red+"Error creating secrets.json file: %v"+Reset, err)
+		return err
+	}
+	fmt.Println("The file was created")
+	return nil
+}
+
+func checkExistenceSecrets() error {
+	_, err := os.Stat(SecretsFileName)
+	if !os.IsNotExist(err) {
+		return nil
+	}
+	fmt.Println(Green + "Secrets file not found, creating..." + Reset)
+
+	return createInitialSecretsFile()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,50 @@
 module github.com/myepes82/crypto_stash
 
 go 1.21.5
+
+require fyne.io/fyne v1.4.3
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
+
+require (
+	fyne.io/fyne/v2 v2.4.3 // indirect
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/fyne-io/mobile v0.1.2 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/jackmordaunt/icns/v2 v2.2.6 // indirect
+	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
+	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/lucor/goinfo v0.9.0 // indirect
+	github.com/mcuadros/go-version v0.0.0-20190830083331-035f6764e8d2 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/cobra v1.8.0
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/urfave/cli/v2 v2.4.0 // indirect
+	golang.org/x/image v0.11.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
